05f2f850a33b63d8dcf944d093a0813eb9e5d27d
Olojakpoke Daniel <holodja@hotmail.com>
2022-08-06 03:11:57 +0100

    fix: update migration

--- database/migrations/sqls/20220613143350-add-config-up.sql
+++ database/migrations/sqls/20220613143350-add-config-up.sql
@@ -1,10 +1,12 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-CREATE TABLE map_config
+CREATE TABLE config
 (
     id uuid NOT NULL PRIMARY KEY DEFAULT uuid_generate_v4(),
-    organization_id uuid NOT NULL UNIQUE,
-    theme jsonb NOT NULL,
+    name character varying(255) NOT NULL,
+    data json NOT NULL,
+    ref_id integer,
+    ref_uuid uuid,
     created_at timestamp without time zone NOT NULL DEFAULT now(),
     updated_at timestamp without time zone NOT NULL DEFAULT now()
 )
