8b5e4dbcccc47583169ea318b5ffa30c45dad62c
keyy123 <parktwins1@gmail.com>
2022-11-10 11:33:48 -0500

    ref_id will not type-cast strings into numbers

--- server/handlers/config/schemas.js
+++ server/handlers/config/schemas.js
@@ -3,7 +3,7 @@ const Joi = require('joi');
 const configPostSchema = Joi.object({
   name: Joi.string().required(),
   data: Joi.object().required(),
-  ref_id: Joi.number().integer(),
+  ref_id: Joi.number().integer().strict(),
   ref_uuid: Joi.string().uuid(),
 });
 
